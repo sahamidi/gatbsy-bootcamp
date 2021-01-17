078ca2801f1f9fe779948ae6194f8279f9cab5fa
Sal Hamidi <hamidisa87@gmail.com>
2021-01-17 08:00:14 -0500

    gatsby first load

---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+let blogPage = () => {
+    return (
+        <div>
+            <h1>
+                Blog Page Holder
+            </h1>
+            <p>
+                This is where we will put the blog information
+            </p>
+        </div>
+    )
+}
+
+export default blogPage
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,17 @@
-import React from "react"
+import React from 'react'
 
-export default function Home() {
-  return <div>Hello world!</div>
+let  IndexPage = () => {
+  return (
+    <div>
+      <h1>
+        Hello.
+      </h1>
+      <h2>
+        Hey there beautiful.  I am Salem Hamidi, a pursuant programmer, 
+        feel free to connect.
+      </h2>
+    </div>
+  )
 }
+
+export default IndexPage 
