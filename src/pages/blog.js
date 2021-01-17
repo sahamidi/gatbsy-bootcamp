358ae181f9f54e5211bc7848dc04943c8d6a4912
Sal Hamidi <hamidisa87@gmail.com>
2021-01-17 09:59:28 -0500

    added blog page

---
+++ src/pages/blog.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+let blogPage = () => {
+    return (
+        <div>
+            <h1>
+                Test Blog Page
+            </h1>
+            <p>
+                Test Content for my blog page.
+            </p>
+        </div>
+    )
+}
+
+export default blogPage
