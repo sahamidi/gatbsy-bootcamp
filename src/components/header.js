5f2173a71ace7e8c78876107749918d31ce14abc
Sal Hamidi <hamidisa87@gmail.com>
2021-01-18 05:52:06 -0500

    added scss styling

--- src/components/header.js
+++ src/components/header.js
@@ -1,16 +1,22 @@
 import React from 'react'
 import { Link } from 'gatsby'
-
+// import './header.module.scss'
+import headerStyles from './header.module.scss'
 
 let Header = () => {
     return (
-       <header>
+       <header className= {headerStyles.header}>
+           <h1>
+               <Link className= {headerStyles.title} to="/">
+                   YourPalSal
+               </Link>
+           </h1>
            <nav>
-               <ul>
-                    <li><Link to="/">Home</Link></li>
-                    <li><Link to="/blog">Blog</Link></li>
-                    <li><Link to="/about">About</Link></li>
-                    <li><Link to="/contact">Contact</Link></li>
+               <ul  className={headerStyles.navList}>
+                    <li><Link className={headerStyles.navItem} to="/">Home</Link></li>
+                    <li><Link className={headerStyles.navItem} to="/blog">Blog</Link></li>
+                    <li><Link className={headerStyles.navItem} to="/about">About</Link></li>
+                    <li><Link className={headerStyles.navItem} to="/contact">Contact</Link></li>
                 </ul>
            </nav> 
         </header>
---
+++ src/pages/pages/contact.js
@@ -0,0 +1,23 @@
+import React from 'react'
+
+import Footer from "../../components/footer"
+
+let contactPage = () => {
+    return (
+        <div>
+            <h1>
+            My Contact Information
+            </h1>
+    <li>Phone: 813-758-4340</li>
+    <li>Email: hamiditechllc@gmail.com</li>
+    <li><a href="https://www.instagram.com/yourpalsal_fl/">Connect with us on IG</a></li>
+    /* <Footer/> */
+        </div>
+
+        
+        
+
+    )
+}
+
+export default contactPage
