4ec2d95899d1cf459be617c243ff4b02bae398d0
Sal Hamidi <hamidisa87@gmail.com>
2021-01-22 08:05:44 -0500

    Need to fix jsx hook

--- src/pages/blog.js
+++ src/pages/blog.js
@@ -1,7 +1,26 @@
 import React from 'react'
 import Layout from '../components/layout'
+import { useStaticQuery, graphql } from 'gatsby'
 
 let blogPage = () => {
+    let data = useStaticQuery(graphql`
+    query{
+        allMarkdownRemark {
+            edges {
+                node {
+                    frontmatter {
+                        title
+                        date
+                    }
+                }
+            }
+        }
+    }
+    `)
+    
+
+console.log(data)
+
     return (
         <Layout>
             <h1>
