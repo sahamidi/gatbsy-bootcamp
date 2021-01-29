48c27d0a61d736828866d16708f5519d8106bdeb
Sal Hamidi <hamidisa87@gmail.com>
2021-01-29 15:09:55 -0500

    Add image to post plugin

--- gatsby-config.js
+++ gatsby-config.js
@@ -7,23 +7,40 @@
  * See: https://www.gatsbyjs.com/docs/gatsby-config/
  */
 
+
+
 module.exports = {
   /* Your site config here */
-  plugins: [
-    'gatsby-plugin-sass',
-    {
-      resolve: 'gatsby-source-filesystem',
-      options: {
-      name: 'src',
-      path: `${__dirname}/src/`,
-      }
-    },
-    `gatsby-transformer-remark`
-  ],
   siteMetadata: {
     title: 'Full-Stack Bootcamp',
     author: 'Salem Hamidi'
   
   },
+  plugins: [
+    'gatsby-plugin-sass',
+    {
+      resolve: 'gatsby-source-filesystem',
+      options: {
+        name: 'src',
+        path: `${__dirname}/src/`,
+      }
+    },
+  'gatsby-plugin-sharp',
+  {
+    resolve: 'gatsby-transformer-remark',
+    options: {
+      plugins: [
+        'gatsby-remark-relative-images',
+        {
+          resolve: 'gatsby-remark-images',
+          options: {
+            maxWidth: 750,
+            linkImagesToOriginal: false
+          }
+        }
+      ]
+    }
+  }
+] 
 
 }
--- src/posts/gatsby.md
+++ src/posts/gatsby.md
@@ -4,6 +4,8 @@ date: "01-20-2021"
 ---
 Why does learning to code matter to me?
 
+![Marcus](./download.jpeg)
+
 ## Topics Covered
 
 1. Gatsby
